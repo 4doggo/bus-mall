2178b5738644bc8df11441436d8bc038a2b5a7a9
shingoed <nakajima.shingo@gmail.com>
2019-11-11 15:08:56 -0800

    good progress

--- app.js
+++ app.js
@@ -4,96 +4,94 @@ var clickCounter = 0;
 var MAX_CLICK_COUNTER = 10;
 
 function getRandomImageIndex() {
-    return Math.floor(Math.random() * (iamgeStorage.length));
+  return Math.floor(Math.random() * (imageStorage.length));
 }
 
 function select3ImagesAndRender() {
-    // select 3 goats :p
-    // we need a loop to select numbers
-    randomImages = [];
+  randomImages = [];
 
-    while (randomImages.length < 3) {
-        var nextRandomValue = getRandomImageIndex();
+  while (randomImages.length < 3) {
+    var nextRandomValue = getRandomImageIndex();
 
-        if (!randomImages.includes(nextRandomValue)) {
-            randomImages.push(nextRandomValue);
-        }
+    if (!randomImages.includes(nextRandomValue)) {
+      randomImages.push(nextRandomValue);
     }
+  }
+}
 
-    var allImages = function (name, picture) {
-        this.name = name;
-        this.picture = picture;
-        this.timesClicked = 0;
-        this.timesShown = 0; // hint hint ;). You may need to use this for your app ;)
+var AllImages = function (name, picture) {
+  this.name = name;
+  this.picture = picture;
+  this.timesClicked = 0;
+  this.timesShown = 0; // hint hint ;). You may need to use this for your app ;)
 
-        this.markClick = function () {
-            this.timesClicked++;
-        }
+  this.markClick = function () {
+    this.timesClicked++;
+  };
 
-        this.render = function (domReference) {
-            domReference.src = picture;
-        }
-        iamgeStorage.push(this);
+  this.render = function (domReference) {
+    domReference.src = picture;
+  };
+  imageStorage.push(this);
+};
+
+var placeholder0 = document.getElementById('placeholder-0');
+var placeholder1 = document.getElementById('placeholder-1');
+var placeholder2 = document.getElementById('placeholder-2');
+// Invariants:
+// randomGoats has 3 goats!
+
+select3ImagesAndRender();
+
+
+imageStorage[randomImages[0]].render(placeholder0);
+imageStorage[randomImages[1]].render(placeholder1);
+imageStorage[randomImages[2]].render(placeholder2);
+
+var bag = new AllImages('bag', './images/bag.jpg');
+var banana = new AllImages('banana', './images/banana.jpg');
+var bathroom = new AllImages('bathroom', './images/bathroom.jpg');
+var boots = new AllImages('boots', './images/boots.jpg');
+var breakfast = new AllImages('breakfast', './images/breakfast.jpg');
+var bubblegum = new AllImages('bubblegum', './images/bubblegum.jpg');
+var chair = new AllImages('chair', './images/chair.jpg');
+var cthulhu = new AllImages('cthulhu', './images/cthulhu.jpg');
+var dogDuck = new AllImages('dog duck', './images/dog-duck.jpg');
+var dragon = new AllImages('dragon', './images/dragon.jpg');
+var pen = new AllImages('pen', './images/pen.jpg');
+var petSweep = new AllImages('pet sweep', './images/pet-sweep.jpg');
+var scissors = new AllImages('scissors', './images/scissors.jpg');
+var shark = new AllImages('shark', './images/shark.jpg');
+var sweep = new AllImages('sweep', './images/sweep.jpg');
+var tauntaun = new AllImages('tauntaun', './images/tauntaun.jpg');
+var unicorn = new AllImages('unicorn', './images/unicorn.jpg');
+var usb = new AllImages('usb', './images/usb.jpg');
+var waterCan = new AllImages('water can', './images/water-can.jpg');
+var wineGlass = new AllImages('wine glass', './images/wine-glass.jpg');
+
+function clickManager(event) {
+  clickCounter++;
+  if (clickCounter < MAX_CLICK_COUNTER) {
+    var imageIndex;
+
+    if (event.target.id === 'placeholder-0') {
+      imageIndex = 0;
+    } else if (event.target.id === 'placeholder-1') {
+      imageIndex = 1;
+    } else {
+      imageIndex = 2;
     }
+    var clickedImage = imageStorage[randomImages[imageIndex]];
+    clickedImage.markClick();
 
-    var placeholder0 = document.getElementById('placeholder-0');
-    var placeholder1 = document.getElementById('placeholder-1');
-    var placeholder2 = document.getElementById('placeholder-2');
-    // Invariants:
-    // randomGoats has 3 goats!
+    select3ImagesAndRender();
+  } else {
+    alert('game over');
+  }
+}
 
-    imageStorage[randomImages[0]].render(placeholder0);
-    imageStorage[randomImages[1]].render(placeholder1);
-    imageStorage[randomImages[2]].render(placeholder2);
 
-    var bag = new Goat('Sweater Goat', './images/bag.jpg');
-    var banana = new Goat('Cruising Goat', './images/banana.jpg');
-    var bathroom = new Goat('Float your Goat', './images/bathroom.jpg');
-    var boots = new Goat('Kissing Goat', './images/boots.jpg');
-    var breakfast = new Goat('Sweater Goat', './images/breakfast.jpg');
-    var bubblegum = new Goat('Cruising Goat', './images/bubblegum.jpg');
-    var chair = new Goat('Float your Goat', './images/chair.jpg');
-    var cthulhu = new Goat('Kissing Goat', './images/cthulhu.jpg');
-    var dogDuck = new Goat('Sweater Goat', './images/dog-duck.jpg');
-    var dragon = new Goat('Cruising Goat', './images/dragon.jpg');
-    var pen = new Goat('Float your Goat', './images/pen.jpg');
-    var petSweep = new Goat('Kissing Goat', './images/pet-sweep.jpg');
-    var scissors = new Goat('Sweater Goat', './images/scissors.jpg');
-    var shark = new Goat('Cruising Goat', './images/shark.jpg');
-    var sweep = new Goat('Float your Goat', './images/sweep.jpg');
-    var tauntaun = new Goat('Kissing Goat', './images/tauntaun.jpg');
-    var unicorn = new Goat('Sweater Goat', './images/unicorn.jpg');
-    var usb = new Goat('Cruising Goat', './images/usb.jpg');
-    var waterCan = new Goat('Float your Goat', './images/water-can.jpg');
-    var wineGlass = new Goat('Kissing Goat', './images/wine-glass.jpg');
 
-    function clickManager(event) {
-        clickCounter++;
-        if (clickCounter < MAX_CLICK_COUNTER) {
-            var imageIndex;
-
-            if (event.target.id === 'placeholder-0') {
-                imageIndex = 0;
-            } else if (event.target.id === 'placeholder-1') {
-                imageIndex = 1;
-            } else {
-                imageIndex = 2;
-            }
-            var clickedImage = imageStorage[randomImages[imageIndex]];
-            clickedImage.markClick();
-
-            select3GoatsAndRender();
-        } else {
-            alert('game over');
-        }
-    }
-
-    select3GoatsAndRender();
-
-    var placeholder0 = document.getElementById('placeholder-0');
-    var placeholder1 = document.getElementById('placeholder-1');
-    var placeholder2 = document.getElementById('placeholder-2');
-
-    placeholder0.addEventListener('click', clickManager);
-    placeholder1.addEventListener('click', clickManager);
-    placeholder2.addEventListener('click', clickManager);
+placeholder0.addEventListener('click', clickManager);
+placeholder1.addEventListener('click', clickManager);
+placeholder2.addEventListener('click', clickManager);
